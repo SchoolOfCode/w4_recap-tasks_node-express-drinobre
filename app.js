8e87510179e1b304d5a97d365bd791c160114e04
drinobre <dri_nobre@hotmail.com>
2021-12-13 10:52:50 +0000

    API routes

--- app.js
+++ app.js
@@ -13,6 +13,57 @@ const app = express();
 
 app.use(express.json());
 
+// create a get request to show all userSelect
+app.get("/", async function (req, res){
+  // get all users
+  const result = await getUsers();
+   // response in json format
+  res.json({success: true, payload: result});
+});
+
+//create a get request to show usersbyid
+app.get("/user/:id", async function (req,res){
+  // convert the id from the path into Number and store in variable 
+  const idNumber = Number(req.params.id)
+  // get user by id
+  const result = await getUserByID(idNumber) 
+  // response in json format
+  res.json({success: true, payload: result});
+});
+
+//create a post request to create user;
+app.post("/user/:id", async function (req, res){
+  //post a request in the body
+  const request = req.body
+  //get user by id including the new request
+  const result = await createUser(request); 
+  //response in json format
+  res.json({success: true, payload: result});
+});
+
+//create a put request to update userbyId;
+app.put("/user/:id", async function (req, res){
+  // convert the id from the path into Number and store in variable 
+  const idNumber = Number(req.params.id);
+  // take the body from the request
+  const request = req.body;
+  // replace the user with id
+  const result = await updateUserByID(idNumber, request);
+  //response in json format
+  res.json({success: true, payload: result});
+});
+
+//create a delete request to delete userbyid; deleteUserByID
+
+app.delete("/user/:id", async function (req, res){
+  // convert the id from the path into Number and store in variable
+  const idNumber = Number(req.params.id);
+  // delete the user with that id
+  const result = await deleteUserByID(idNumber);
+  //response in json format
+  res.json({success: true, payload: result});
+})
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
--- models/users.js
+++ models/users.js
@@ -2,7 +2,7 @@ import users from "../data/users.js";
 
 // GET ALL USERS
 // return all user users from users array
-export function getUsers() {
+export async function getUsers() {
     return users;
 }
 //console.log(getUsers());
@@ -11,7 +11,7 @@ export function getUsers() {
 // GET A USER BY ID
 // find user id on the array
 // return array for the specific user id - Done
-export function getUserByID(id){
+export async function getUserByID(id){
     let result = users.find(function isUserIdFound(user){
     return user.id === id;
 });
@@ -20,11 +20,11 @@ return result;
 
 //Note: if the id is not found on the array the result will be undefined;
 
-console.log(getUserByID(3));
+//console.log(getUserByID(3));
 
 
 //CREATE A USER
-export function createUser(newUser) {
+export async function createUser(newUser) {
     users.push(newUser);
     return users;
 }
@@ -35,7 +35,7 @@ export function createUser(newUser) {
 //   }));
 
 // UPDATE A USER BY ID
-export function updateUserByID(id, updatedUser) {
+export async function updateUserByID(id, updatedUser) {
    
     // Find index of specific object using findIndex method.
     let userIndex = users.findIndex(function isUseridFound(user){
@@ -58,7 +58,7 @@ export function updateUserByID(id, updatedUser) {
 
 
 // DELETE A USER BY ID
-export function deleteUserByID(id) {
+export async function deleteUserByID(id) {
     // Filter out the userbyId using the filter method
     let userIndex = users.filter(function isUseridFound(user){
         return user.id !== id;
@@ -67,4 +67,4 @@ export function deleteUserByID(id) {
     return userIndex;
 }
 
-console.log(deleteUserByID(50));
+//console.log(deleteUserByID(50));
